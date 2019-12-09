9d9d27a8a47e37cc883247ce43f7dd5656dfbb60
Jantunen <hannajant@gmail.com>
2019-12-09 21:18:13 +0200

    vko 6 teht 5 loppuun

--- Viikko 6/QueryLanguage/src/main/java/statistics/QueryBuilder.java
+++ Viikko 6/QueryLanguage/src/main/java/statistics/QueryBuilder.java
@@ -17,17 +17,17 @@ class QueryBuilder {
     }
 
     public QueryBuilder playsIn(String team) {
-        this.m = new PlaysIn(team);
+        this.m = new And(this.m, new PlaysIn(team));
         return this;
     }
 
     public QueryBuilder hasAtLeast(int value, String category) {
-        this.m = new HasAtLeast(value, category);
+        this.m = new And(this.m, new HasAtLeast(value, category));
         return this;
     }
 
     public QueryBuilder hasFewerThan(int value, String category) {
-        this.m = new HasFewerThan(value, category);
+        this.m = new And(this.m, new HasFewerThan(value, category));
         return this;
     }
 }
